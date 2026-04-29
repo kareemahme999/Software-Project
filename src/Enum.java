e5c26bde0460ac8f41d6044afb1911d934e81387
Kareem Ahmed <kareemahmed4795@gmail.com>
2026-04-29 22:46:47 +0300

    make a model

---
+++ src/Enum.java
@@ -0,0 +1,16 @@
+ enum NotificationType {
+    BUDGET_NEAR_LIMIT,
+    BUDGET_EXCEEDED,
+    GOAL_COMPLETED
+}
+ enum BudgetStatus {
+    ON_TRACK,
+    NEAR_LIMIT,
+    EXCEEDED
+}
+ enum GoalStatus {
+    IN_PROGRESS,
+    NEAR_LIMIT,
+    COMPLETED,
+    CANCELLED
+}
